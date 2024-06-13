668590c6f6dd62175c08d59a477839e3a4b7c988
David Weaver <davidwweaver3060@gmail.com>
2024-06-13 09:28:47 -0700

    function predict changes

--- main/predict.py
+++ main/predict.py
@@ -2,23 +2,35 @@ import numpy as np
 from tensorflow.keras.models import load_model
 import matplotlib.pyplot as plt
 
-def make_custom_prediction(model, input_data):
-    # Convert input data to a NumPy array and reshape it for model input
+# -------------------------------------THIS FUNCTION ROUNDS THE PREDICTION TO THE NEAREST WHOLE INTEGER--------------------------------------
+
+# note: change the function to return floating point numbers when the data recieved from the user is also a floating point number
+
+def make_custom_prediction(model, input_data):             
+    
     input_data = np.array(input_data).reshape((1, len(input_data), 1))
     
-    # Make prediction on the input data
     prediction = model.predict(input_data)
     
-    # Convert prediction to whole integer
     prediction_rounded = int(np.round(prediction))
     
     return prediction_rounded
 
+# ---------------------------------------------------------------------------
 
-model = load_model("models/my_model_v3.h5")
+#def make_custom_prediction(model, input_data):
+    
+    #input_data = np.array(input_data).reshape((1, len(input_data), 1))
+    
+    #prediction = model.predict(input_data)
+    
+    #return prediction[0][0]  # Return the raw predicted value
+
+
+model = load_model("C:/Users/Administrator/func pred/function_prediction/models/my_model_v3.h5")
 
 x = 0
-original_points = a,b,c,d= 1,4,9,16 # these are the points the user sends in, given that the first point is 1
+original_points = a,b,c,d= 1,2,1,2 # these are the points the user sends in, given that the first point is 1
 data_plot = [a,b,c,d]
 input_data = [a,b,c,d]
 x_values = [1,2,3,4]
