35143cbac0ae3047e9a5adb76054f05ef56af459
David Weaver <davidwweaver3060@gmail.com>
2024-06-29 16:26:53 -0700

    Used mostly for testing

--- main/testplot.py
+++ main/testplot.py
@@ -1,116 +1,71 @@
-import math
+import itertools
 import numpy as np
 import matplotlib.pyplot as plt
-from math import pow
 
 # ----------------------- THE CONTROL PANEL -----------------------  
-x = [3,18,22,25,35,41]  # x values from the user
-y = [1,5,8,15,25,40] # y values from the user
+x = [3,18,2,25,35,41]  # x values from the user
+y = [1,5,8,15,25,40]   # y values from the user
 
-# x = [4,2,1,5,6]
-# y = [16,4,1,25,36]
+#x = [-20,-10,0,10,22,30]
+#y = [20,9,7,14,25,0]
 
-x_graph_limit_low = -5.5
-x_graph_limit_high = 2
-y_graph_limit_low = -260
+
+x_graph_limit_low = -10
+x_graph_limit_high = 1.2
+y_graph_limit_low = -5.5
 y_graph_limit_high = 1.6
 
-
-# -----------------------------------------------------------------
-
+n = 2 # degree of polynomial
 
 def append_row(matrix, new_row):  # function to append rows into matrix
     return np.vstack([matrix, new_row])
 
+def fit_polynomial(x_points, y_points, degree):
+    A = np.vander(x_points, degree + 1)
+    b = np.array(y_points)
+    coeffs = np.linalg.lstsq(A, b, rcond=None)[0]
+    return coeffs
+# Generate all combinations of 3 points out of 6
+combinations = list(itertools.combinations(range(len(x)), 3))
 
-
-
-n = 2 # degree of polynomial
-
-
-
-A = np.empty((0, n+1))
-b = np.empty((0, 1))
-
-
-i = 1   # Outer while loop that appends the matrix
-while i != n+2:  # this while loop takes any degree polynomial and builds an n+1 x n+1 matrix (to solve system of equations, matrix of x's), along with returning the b vector (column of y)
-    b = np.append(b, [[y[i-1]]], axis=0)
-    row = []
-    k = 0   # Power indexing
-    g = 1   # Very inner while loop that populates rows
-    while g != n+2:
-        row.append(pow(x[i-1], n-k)) # starts with row highest power of polynomial, ex: x_2^2  x_2^1 x_2^0
-        k += 1
-        g += 1
-
-    row = np.array(row).reshape(1, -1)
-    A = append_row(A, row)
-    i += 1
-
-
-# Calculate the inverse of A
-A_inv = np.linalg.inv(A)
-
-result = A_inv @ b
-
-
-
-i=0
-solutions = []
-while i != n+1:
-    #solutions.append(np.round(result[i, 0], decimals = 2))
-    solutions.append(result[i, 0])
-    i=i+1
-
-
-# i=0     FOR ROUNDING, HOWEVER IT WONT WORK WITH CERTAIN POINTS.
-# while i != n+1:
-#     if abs(solutions[i]) < 0.01 and solutions[i] >= 0:
-#         solutions[i] = 0
-#     i=i+1
-
-
-# Check if values are less than 0.1, round them
-
-#print("Your function: ", solutions[0], "x^5 + ", solutions[1], "x^4 + ", solutions[2], "x^3 + ", solutions[3], "x^2 + ", solutions[4], "x + ", solutions[5])
-
-
-
-x_n = np.linspace(x[0], x[5], 400) # create 400 points between x_1 and x_f
-x_forward = np.linspace(x[5]+0.1, 50, 400)
-x_backward = np.linspace(-50, x[0]-0.1, 400)
-x_n = np.append(x_n, x_forward)
-x_n = np.insert(x_n, 0, x_backward)
-
-# Compute the polynomial values for each x
-y_n = np.polyval(solutions, x_n)  #Plots any degree polynomial using A,B,C,D,E ... solutions
-
-# need to grab the highest and lowest x and y points 
-x_max = np.max(x)
+# Common x values to evaluate the polynomial on
 x_min = np.min(x)
+x_max = np.max(x)
 y_max = np.max(y)
 y_min = np.min(y)
+x_common = np.linspace(x_min, x_max, 400)
 
+# List to store all the polynomial values
+all_y_values = []
+
+for combo in combinations:
+    x_points = [x[i] for i in combo]
+    y_points = [y[i] for i in combo]
+    coeffs = fit_polynomial(x_points, y_points, n)
+    y_values = np.polyval(coeffs, x_common)
+    all_y_values.append(y_values)
+
+# Average the y values from all polynomials
+y_average = np.mean(all_y_values, axis=0)
+
+# Plotting
 plt.figure(figsize=(8, 6))
 plt.scatter(x, y, color='blue', alpha=0.6, marker='o', label='Your Points',zorder=2)
+plt.plot(x_common, y_average, color='red', label='Average Graph', zorder=1)
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
-plt.title('User Points')
+plt.title('Averaged Graph')
 plt.grid(True)
-plt.xlim(x_min*x_graph_limit_low, x_max*x_graph_limit_high) # for limits on the plots
-plt.ylim(y_min*y_graph_limit_low, y_max*y_graph_limit_high)
+plt.xlim(abs(x_min)*x_graph_limit_low, abs(x_max)*x_graph_limit_high) # for limits on the plots
+plt.ylim(abs(y_min)*y_graph_limit_low, abs(y_max)*y_graph_limit_high)
+plt.legend()
 plt.show()
 
 plt.figure(figsize=(8, 6))
-plt.plot(x_n,y_n, color='red', label='New Graph',zorder=1)
-plt.scatter(x,y, color='blue', alpha=0.6, marker='o',zorder=2)
+plt.plot(x_common, y_average, color='red', label='Average Graph',zorder=1)
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
-plt.title('Plotted Graph')
+plt.title('Full Graph')
 plt.grid(True)
-plt.xlim(x_min*x_graph_limit_low, x_max*x_graph_limit_high) # for limits on the plots
-plt.ylim(y_min*y_graph_limit_low, y_max*y_graph_limit_high)
+plt.legend()
 plt.show()
-
-# -------------------------------------------------------
