0409ef344dc3a1e429741ff01ff9266929a9988e
David Weaver <davidwweaver3060@gmail.com>
2024-07-05 12:43:36 -0700

    No Significant Change

--- main/main.py
+++ main/main.py
@@ -7,8 +7,6 @@ import sys
 x = [-4, -3.5, -2.8, -1.6, -0.9, 0.0, 1.2]  # x values from the user
 y = [0.5, 1.1, 2.3, 4.7, 9.2, 15.0, 27.5] # y values from the user
 
-
-
 n = 2 # degree of polynomial
 
 func.poly_avgerage(x,y,n)
