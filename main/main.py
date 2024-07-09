804ef7ab8eecec6e76041f32e22dcf5e333bae7a
David Weaver <davidwweaver3060@gmail.com>
2024-07-08 17:58:12 -0700

    Introduced prediction in the main (model)

--- main/main.py
+++ main/main.py
@@ -4,21 +4,18 @@ from math import pow
 import func
 import sys
 
-x = [-4, -3.5, -2.8, -1.6, -0.9, 0.0, 1.2]  # x values from the user
-y = [0.5, 1.1, 2.3, 4.7, 9.2, 15.0, 27.5] # y values from the user
+x = [77, 78, 79, 80, 81, 82]
+y = [78, 79, 80, 81, 82, 83]
+predicted_function = func.predict_function(x,y)
 
-# x = [-3.0, -2.1, -1.5, -0.7, 0.0, 0.8, 1.5, 2.3, 3.0, 4.0]
-# y = [5.0, 8.5, 2.3, 1.8, 1.0, 2.5, 3.2, 10.0, 20.1, 55.0]
+print(f'Your function: {predicted_function}')
+n = 2 # degree of polynomial
 
-
-# x = np.linspace(0, 10, 100)
-# y = np.sin(x) + np.random.normal(0, 0.1, 100)  # Simulated noisy sine wave
-
-x=[-1,-2,-3,-4,-5,-6]
-y=[2,1.5,0.8,0.4,0.2,0.1]
-
-n = 4 # degree of polynomial
-
-#func.poly_avgerage(x,y,n)
-#func.sine_average(x,y)
-func.exp_average(x,y)
+if predicted_function == "polynomial":
+    func.poly_avgerage(x,y,n)
+if predicted_function == "sine":
+    func.sine_average(x,y)
+if predicted_function == "exponential":
+    func.exp_average(x,y)
+if predicted_function == "linear":
+    func.poly_avgerage(x,y,1)
