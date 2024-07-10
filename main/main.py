a608aabb013d46f8870207d931deda3d52e9732e
David Weaver <davidwweaver3060@gmail.com>
2024-07-09 18:34:25 -0700

    No sig change

--- main/main.py
+++ main/main.py
@@ -4,14 +4,14 @@ from math import pow
 import func
 import sys
 
-x = [-2.0, -1.0, 0.0, 1.0, 2.0, 3.0]
-y = [35.0, 8.0, 1.0, -2.0, 3.0, 46.0]
+x = [9,27,19,16,100,90]
+y = [54,72,67,129,651,9]
 
 
 predicted_function = func.predict_function(x,y)
 
 print(f'Your function: {predicted_function}')
-n = 4 # degree of polynomial
+n = 5 # degree of polynomial
 
 
 if predicted_function == "polynomial":
@@ -20,4 +20,3 @@ if predicted_function == "sine":
     func.sine_average(x,y)
 if predicted_function == "exponential":
     func.exp_average(x,y)
-
