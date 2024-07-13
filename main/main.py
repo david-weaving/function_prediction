d4d2235a9859cbba3ec47f0b1524771089096610
David Weaver <davidwweaver3060@gmail.com>
2024-07-12 18:11:01 -0700

    No Sig Change

--- main/main.py
+++ main/main.py
@@ -7,16 +7,17 @@ import sys
 x = [1,2,3,4,5,6]
 y = [1,4,9,16,25,36]
 
-x = [-3.0, -1.0, 0.0, 1.0, 2.0, 4.0]
-y = [-0.28, 1.83, 2.0, 1.83, 0.28, -1.58]
-func.sine_average(x,y)
+x = [-3.00, -1.50, 0.00, 1.50, 3.00, 4.50]
+y = [12.50, 4.75, 1.00, 2.75, 4.50, 5.75]
+
+
 predicted_function = func.predict_function(x,y)
 
 
 
 print(f'Your function: {predicted_function}')
 
-n = 2 # degree of polynomial
+n = 3 # degree of polynomial
 
 if predicted_function == "polynomial":
     func.poly_avgerage(x,y,n)
