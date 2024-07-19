bf86ae96495f8dd8a3fe68ef66caadd0eaa4544b
David Weaver <davidwweaver3060@gmail.com>
2024-07-18 22:56:56 -0700

    No Sig Change

--- model training sets/model_training_func_type.py
+++ model training sets/model_training_func_type.py
@@ -32,7 +32,7 @@ x_train_reshaped = x_train.reshape((len(y_train), 6, 2))
 
 # Split data into training and validation sets
 x_train_split, x_val_split, y_train_split, y_val_split = train_test_split(
-    x_train_reshaped, y_train_encoded, test_size=0.2, random_state=11) # 10 is good
+    x_train_reshaped, y_train_encoded, test_size=0.2, random_state=22) # 10 is good
 
 # l=0
 # p=0
@@ -66,18 +66,18 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-batch_size=10
+batch_size=15
 steps_per_epoch = len(x_train_reshaped) // batch_size  # Batch size is 1, so we use the whole dataset in each epoch
 
 
 # Train the model with validation data
 history = model.fit(x_train_reshaped, y_train_encoded, 
                     validation_data=(x_val_split, y_val_split),
-                    epochs=1500, batch_size=batch_size, steps_per_epoch=steps_per_epoch)
+                    epochs=2500, batch_size=batch_size, steps_per_epoch=steps_per_epoch)
 
 # Evaluate model performance on validation data
 val_loss, val_acc = model.evaluate(x_val_split, y_val_split)
 print(f"Validation accuracy: {val_acc}")
 
 # Save the model
-model.save("models/model_V1_6.h5")
+model.save("C:/Users/Administrator/func pred/function_prediction/models/model_V1_6.h5")
