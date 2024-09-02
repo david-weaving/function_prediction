0cba3869887b67c676bc8350cdd8388d56904b5c
David Weaver <davidwweaver3060@gmail.com>
2024-09-02 11:40:15 -0700

    New Commit

--- model training sets/model_training_degree.py
+++ model training sets/model_training_degree.py
@@ -26,15 +26,15 @@ print(f'Number of FIRST degrees: {k}, Number of SECOND degrees: {p}, Number of T
 exit()
 
 
-# Encode labels
+# labels
 label_map = {"1": 0, "2": 1, "3": 2, "4": 3, "5": 4}
 y_train_encoded = np.array([label_map[label[0]] for label in y_train_degree])
 
 
-# Reshape x_train to fit the model input
+# reshape x_train to fit the model input
 x_train_reshaped = x_train_degree.reshape((len(y_train_degree), 6, 2))
 
-# Split data into training and validation sets
+# split data
 x_train_split, x_val_split, y_train_split, y_val_split = train_test_split(
     x_train_reshaped, y_train_encoded, test_size=0.2, random_state=22) # 10 is good
 
@@ -57,7 +57,7 @@ x_train_split, x_val_split, y_train_split, y_val_split = train_test_split(
 
 # exit()
 
-# Define the model with some changes
+# the model
 model = tf.keras.models.Sequential([
     tf.keras.layers.LSTM(128, return_sequences=True, input_shape=(6, 2)),
     tf.keras.layers.LSTM(64),
@@ -66,24 +66,24 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(5, activation='softmax')
 ])
 
-# Compile the model
+# compile
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# Define callbacks
+
 early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.0001)
 
-# Train the model
+# for training
 history = model.fit(x_train_reshaped, y_train_encoded, 
                     validation_data=(x_val_split, y_val_split),
                     epochs=1000, batch_size=32,
                     callbacks=[early_stopping, reduce_lr])
 
-# Evaluate model performance on validation data
+# performance
 val_loss, val_acc = model.evaluate(x_val_split, y_val_split)
 print(f"Validation accuracy: {val_acc}")
 
-# Save the model
+# save
 model.save("C:/Users/Administrator/func pred/function_prediction/models/model_degree_V1.h5")
